be1cf5a81c93deeffb10714d6cfaec1088e13cec
tautcony <tautcony@gmail.com>
2015-12-30 16:33:01 +0800

    code style improvement

--- AutoTorrentInspection/Util/TorrentData.cs
+++ AutoTorrentInspection/Util/TorrentData.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text;
 using BencodeNET;
 using BencodeNET.Objects;
+using System.Collections.Generic;
 
 namespace AutoTorrentInspection.Util
 {
@@ -19,7 +19,7 @@ namespace AutoTorrentInspection.Util
         public IEnumerable<string> GetAnnounceList()
         {
             var announceList = _torrent.AnnounceList;
-            return announceList?.ToList().Select(item => ((BList)item).First().ToString()).ToList() ?? new List<string>() { _torrent.Announce };
+            return announceList?.ToList().Select(item => ((BList)item).First().ToString()).ToList() ?? new List<string> { _torrent.Announce };
         }
 
         public string TorrentName => _torrent.Info["name"].ToString();
@@ -32,27 +32,26 @@ namespace AutoTorrentInspection.Util
             var files = (BList)_torrent.Info["files"];
             if (files == null)
             {
-                var name = _torrent.Info["name"].ToString();
-                string fileExt = Path.GetExtension(name).ToLower();
-                var length = ((BNumber)_torrent.Info["length"]).Value;
-                fileDic.Add("", new List<FileDescription>());
-                fileDic[""].Add(new FileDescription
+                var name    = _torrent.Info["name"].ToString();
+                var fileExt = Path.GetExtension(name).ToLower();
+                var length  = ((BNumber)_torrent.Info["length"]).Value;
+                fileDic.Add("singe", new List<FileDescription>());
+                var fd = new FileDescription
                 {
                     FileName = name,
-                    Path = "",
-                    Ext = fileExt,
-                    Category = "",
-                    Length = length,
-                });
+                    Path     = "",
+                    Ext      = fileExt,
+                    Length   = length,
+                };
+                fd.CheckValid();
+                fileDic["singe"].Add(fd);
                 return fileDic;
             }
-
-
             foreach (var bObject in files)
             {
                 var singleFile = (BList)((BDictionary)bObject)["path"];
                 var length     = ((BNumber)((BDictionary)bObject)["length"]).Value;
-                var category = singleFile.Count != 1 ? singleFile.First().ToString() : "root";
+                var category   = singleFile.Count != 1 ? singleFile.First().ToString() : "root";
                 var path = new StringBuilder();
                 for (int i = 0; i < singleFile.Count - 1; i++)
                 {
@@ -65,14 +64,15 @@ namespace AutoTorrentInspection.Util
                 {
                     fileDic.Add(category, new List<FileDescription>());
                 }
-                fileDic[category].Add(new FileDescription
+                var fd = new FileDescription
                 {
                     FileName = fileName,
-                    Path = path.ToString(),
-                    Ext = fileExt,
-                    Category = category,
-                    Length = length,
-                });
+                    Path     = path.ToString(),
+                    Ext      = fileExt,
+                    Length   = length,
+                };
+                fd.CheckValid();
+                fileDic[category].Add(fd);
             }
             return fileDic;
         }
