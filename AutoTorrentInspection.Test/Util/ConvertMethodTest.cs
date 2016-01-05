3563d6540fd83dde77090ee2c405660fd4e3ce89
tautcony <tautcony@gmail.com>
2016-01-05 22:05:20 +0800

    add a unit test for some function

---
+++ AutoTorrentInspection.Test/Util/ConvertMethodTest.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using AutoTorrentInspection.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoTorrentInspection.Test.Util
+{
+    [TestClass()]
+    public class ConvertMethodTest
+    {
+        [TestMethod()]
+        public void IsUTF8Test()
+        {
+            var utfCount = Directory.GetFiles( @"C:\Users\TautCony\Documents\auto-torrent-inspection\AutoTorrentInspection.Test\[Encode Sample]")
+                    .Sum(textFile => ConvertMethod.IsUTF8(textFile) ? 1 : 0);
+            Assert.IsTrue(utfCount == 3);
+        }
+
+        [TestMethod()]
+        public void GetFileListTest()
+        {
+            var result = ConvertMethod.GetFileList(@"C:\Users\TautCony\Documents\auto-torrent-inspection\AutoTorrentInspection.Test\[Folder Sample]");
+            Assert.IsTrue(result.Count              == 3);
+            Assert.IsTrue(result["root"].Count      == 1);
+            Assert.IsTrue(result["[folder1]"].Count == 3);
+            Assert.IsTrue(result["[folder2]"].Count == 3);
+        }
+    }
+}
---
+++ AutoTorrentInspection.Test/[Encode Sample]/GB2312.cue
@@ -0,0 +1,20 @@
+REM GENRE Anime
+REM DATE 2012
+TITLE "夏色キセキ 3【完全生産限定版】特典CD"
+FILE "CDImage.flac" WAVE
+  TRACK 01 AUDIO
+    TITLE "初想いDay"
+    PERFORMER "花木優香(戸松遥), 環凛子(豊崎愛生)"
+    INDEX 01 00:00:00
+  TRACK 02 AUDIO
+    TITLE "空飛ぶクジラ"
+    PERFORMER "環凛子(豊崎愛生)"
+    INDEX 01 04:13:53
+  TRACK 03 AUDIO
+    TITLE "初想いDay -カラオケver.-"
+    PERFORMER "rino"
+    INDEX 01 06:40:30
+  TRACK 04 AUDIO
+    TITLE "空飛ぶクジラ -カラオケver.-"
+    PERFORMER "多羅尾伴内"
+    INDEX 01 10:54:08
