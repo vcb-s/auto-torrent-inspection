9b5f6459dc8c7973877276be350626985497fdfd
tautcony <tautcony@gmail.com>
2016-02-04 14:01:25 +0800

    assert position at end of the string for the extension detection.

--- AutoTorrentInspection/Util/FileDescription.cs
+++ AutoTorrentInspection/Util/FileDescription.cs
@@ -22,8 +22,8 @@ namespace AutoTorrentInspection.Util
         public override string ToString() => $"{FileName}, length: {(double)Length / 1024:F3}KB";
 
         private static readonly Regex AnimePartten  = new Regex(@"^\[[^\[\]]*VCB-S(?:tudio)*[^\[\]]*\] [^\[\]]+ (\[.*\d*\])*\[((?<Ma>Ma10p_1080p)|(?<Hi>(Hi10p|Hi444pp)_(1080|720|480)p)|(?<EIGHT>(1080|720)p))\]\[((?<HEVC-Ma>x265)|(?<AVC-Hi>x264)|(?(EIGHT)x264))_\d*(flac|aac|ac3)\](?<SUB>(\.(sc|tc)|\.(chs|cht))*)\.((?(AVC)(mkv|mka|flac))|(?(HEVC)(mkv|mka|flac)|(?(EIGHT)mp4))|(?(SUB)ass))$");
-        private static readonly Regex MusicPartten  = new Regex(@"\.(flac|tak|m4a|cue|log|jpg|jpeg|jp2)");
-        private static readonly Regex ExceptPartten = new Regex(@"\.(rar|7z|zip)");
+        private static readonly Regex MusicPartten  = new Regex(@"\.(flac|tak|m4a|cue|log|jpg|jpeg|jp2)$");
+        private static readonly Regex ExceptPartten = new Regex(@"\.(rar|7z|zip)$");
 
         public static FileDescription CreateWithCheckTorrent(string fileName, string reletivePath, long length)
         {
