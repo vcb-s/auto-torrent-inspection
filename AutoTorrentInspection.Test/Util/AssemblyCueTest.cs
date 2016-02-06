57fbc2f31da2ab2e39d1c6e3bd2cb19c12bfda1b
tautcony <tautcony@gmail.com>
2016-02-06 19:37:48 +0800

    Port two function from C++ to extract cue from flac and tak.

---
+++ AutoTorrentInspection.Test/Util/AssemblyCueTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoTorrentInspection.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTorrentInspection.Util.Tests
+{
+    [TestClass()]
+    public class AssemblyCueTest
+    {
+        [TestMethod()]
+        public void GetCueTest()
+        {
+            const string takPath  = @"D:\Video\[VCB-S&philosophy-raws][K-ON! The Movie]\CDs\[EAC][111207] 映画「けいおん!」OP「Unmei♪wa♪Endless！」／放課後ティータイム\Unmei♪wa♪Endless！.tak";
+            const string flacPath = @"D:\Code\@AutoTorrentInspection\GNCA-0330.Flac";
+            Console.WriteLine(AssemblyCue.GetCueFromTak(takPath));
+            Console.WriteLine(AssemblyCue.GetCueFromFlac(flacPath));
+        }
+    }
+}
---
+++ AutoTorrentInspection/Util/AssemblyCue.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace AutoTorrentInspection.Util
+{
+    public static class AssemblyCue
+    {
+        /// <summary>
+        /// 从含有CueSheet的的区块中读取cue
+        /// </summary>
+        /// <param name="buffer">含有CueSheet的区块</param>
+        /// <param name="type">音频格式类型</param>
+        /// <returns>UTF-8编码的cue</returns>
+        private static string GetCueSheet(byte[] buffer, string type)
+        {
+            type = type.ToLower();
+            if (type != "flac" && type != "tak")
+            {
+                throw new ArgumentException(nameof(type));
+            }
+            int length = buffer.Length;
+            //查找 Cuesheet 标记,自动机模型,大小写不敏感
+            int state = 0, beginPos = 0;
+            for (int i = 0; i < length; ++i)
+            {
+                if ((buffer[i] >= 0x41) && (buffer[i] <= 0x5A))
+                    buffer[i] = (byte)(buffer[i] + 0x20);
+
+                switch ((char)buffer[i])
+                {
+                    case 'c':
+                        state = 1;      //C
+                        break;
+                    case 'u':
+                        state = state == 1 ? 2 : 0;//Cu
+                        break;
+                    case 'e':
+                        switch (state)
+                        {
+                            case 2:
+                                state = 3;  //Cue
+                                break;
+                            case 5:
+                                state = 6;  //Cueshe
+                                break;
+                            case 6:
+                                state = 7;  //Cueshee
+                                break;
+                            default:
+                                state = 0;
+                                break;
+                        }
+                        break;
+                    case 's':
+                        state = state == 3 ? 4 : 0;//Cues
+                        break;
+                    case 'h':
+                        state = state == 4 ? 5 : 0;//Cuesh
+                        break;
+                    case 't':
+                        state = state == 7 ? 8 : 0;//Cuesheet
+                        break;
+                    default:
+                        state = 0;
+                        break;
+                }
+                if (state != 8) continue;
+                beginPos = i + 2;
+                break;
+            }
+            int controlCount = type == "flac" ? 3 : type == "tak" ? 6 : 0;
+            int endPos = 0;
+            state = 0;
+            //查找终止符 0D 0A ? 00 00 00 (连续 controlCount 个终止符以上) (flac为3, tak为6)
+            for (int i = beginPos; i < length; ++i)
+            {
+                switch (buffer[i])
+                {
+                    case 0:
+                        state++;
+                        break;
+                    default:
+                        state = 0;
+                        break;
+                }
+                if (state != controlCount) continue;
+                endPos = i - controlCount; //指向0D 0A后的第一个字符
+                break;
+            }
+            if (beginPos == 0 || endPos <= 1) return string.Empty;
+
+            if ((buffer[endPos - 2] == '\x0D') && (buffer[endPos - 1] == '\x0A'))
+                endPos--;
+
+            var cueLength = endPos - beginPos + 1;
+            if (cueLength <= 10) return string.Empty;
+            string cueSheet = Encoding.UTF8.GetString(buffer, beginPos, cueLength);
+            Debug.WriteLine(cueSheet);
+
+            return cueSheet;
+        }
+
+        public static string GetCueFromTak(string takPath)
+        {
+            var fs = File.Open(takPath, FileMode.Open);
+            if (fs.Length < 1048576)// 小于1M，文档太小了
+            {
+                fs.Close();
+                return string.Empty;
+            }
+            var header = new byte[4];
+            fs.Read(header, 0, 4);
+            if (Encoding.ASCII.GetString(header, 0, 4) != "tBaK")
+            {
+                fs.Close();
+                throw new InvalidDataException($"Except an flac but get an {Encoding.ASCII.GetString(header, 0, 4)}");
+            }
+            fs.Seek(-20480, SeekOrigin.End);
+            var buffer = new byte[20480];
+            fs.Read(buffer, 0, 20480);
+            fs.Close();
+            return GetCueSheet(buffer, "tak");
+        }
+
+        public static string GetCueFromFlac(string flacPath)
+        {
+            var fs = File.Open(flacPath, FileMode.Open);
+            if (fs.Length < 1048576)// 小于1M，文档太小了
+            {
+                fs.Close();
+                return string.Empty;
+            }
+            var header = new byte[4];
+            fs.Read(header, 0, 4);
+            if (Encoding.ASCII.GetString(header, 0, 4) != "fLaC")
+            {
+                fs.Close();
+                throw new InvalidDataException($"Except an flac but get an {Encoding.ASCII.GetString(header, 0, 4)}");
+            }
+
+            var buffer = new byte[1];
+            //4个字节的METADATA_BLOCK_HEADER
+            do
+            {
+                int length;
+                fs.Read(header, 0, 4);
+                //解析
+                //检查最高位是否为1
+                if ((header[0] & 0x80) == 0x80)
+                {
+                    //最后一个METADATA_BLOCK
+                    if ((header[0] & 0x7F) == 0x04)//是VORBIS_COMMENT
+                    {
+                        //读取BLOCK长度
+                        length = header[1] * 0x10000 + header[2] * 0x100 + header[3];
+                        buffer = new byte[length];
+                        //读取BLOCK DATA
+                        fs.Read(buffer, 0, length);
+                    }
+                    break;
+                }
+                //不是最后一个METADATA_BLOCK
+                if ((header[0] & 0x7F) == 0x04)//是VORBIS_COMMENT
+                {
+                    //读取BLOCK长度
+                    length = header[1] * 0x10000 + header[2] * 0x100 + header[3];
+                    buffer = new byte[length];
+                    //读取BLOCK DATA
+                    fs.Read(buffer, 0, length);
+                    break;
+                }
+                //读取BLOCK长度
+                length = header[1] * 0x10000 + header[2] * 0x100 + header[3];
+                //移动文件指针
+                fs.Seek(length, SeekOrigin.Current);
+            } while (fs.Position <= 1048576L);
+            fs.Close();
+
+            return GetCueSheet(buffer, "flac");
+        }
+    }
+}
