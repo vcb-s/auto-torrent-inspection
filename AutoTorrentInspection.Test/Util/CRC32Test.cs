3d755d40fc15d86a5bc395d3404bd87f58a2050a
tautcony <tautcony@gmail.com>
2016-02-27 21:30:10 +0800

    Add CRC32(not in using now.

---
+++ AutoTorrentInspection.Test/Util/CRC32Test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoTorrentInspection.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTorrentInspection.Util.Tests
+{
+    [TestClass()]
+    public class CRC32Test
+    {
+        [TestMethod()]
+        public void FileCRCTest()
+        {
+            string path = @"E:\AVS\embrace\VTS_01_1 [B2708D30].VOB";
+            Console.WriteLine(CRC32.FileCRC(path));
+            uint crc;
+            CRC32.FindCRC(path,out crc);
+            Console.WriteLine(crc);
+        }
+    }
+}
