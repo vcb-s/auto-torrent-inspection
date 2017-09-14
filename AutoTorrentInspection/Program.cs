9572da2a82c67cd9d58464f0dbba811c380c7474
tautcony <tautcony@gmail.com>
2017-09-14 10:29:19 +0800

    Check dependencies while loading.

--- AutoTorrentInspection/Program.cs
+++ AutoTorrentInspection/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using AutoTorrentInspection.Forms;
 using AutoTorrentInspection.Logging.Handlers;
 using Microsoft.Win32;
+using AutoTorrentInspection.Util;
 
 namespace AutoTorrentInspection
 {
@@ -19,14 +21,19 @@ namespace AutoTorrentInspection
 
             if (!IsSupportedRuntimeVersion())
             {
-                var ret = Util.Notification.ShowInfo("需要 .Net4.7 或以上版本以保证所有功能正常运作，是否不再提示？");
+                var ret = Notification.ShowInfo("需要 .Net4.7 或以上版本以保证所有功能正常运作，是否不再提示？");
                 System.Diagnostics.Process.Start("http://dotnetsocial.cloudapp.net/GetDotnet?tfm=.NETFramework,Version=v4.7");
-                if (ret == DialogResult.Yes) Util.RegistryStorage.Save("False", name: "DoVersionCheck");
+                if (ret == DialogResult.Yes) RegistryStorage.Save("False", name: "DoVersionCheck");
             }
 
             Logger.StoreLogMessages = true;
             Logger.LoggerHandlerManager.AddHandler(new DebugConsoleLoggerHandler());
 
+            if (!CheckDependencies())
+            {
+                //todo: download the dependencies automatically
+                return;
+            }
             if (args.Length == 0)
             {
                 Application.Run(new Form1());
@@ -53,6 +60,26 @@ namespace AutoTorrentInspection
                 }
             }
             return false;
+        }
+
+
+        private static bool CheckDependencies()
+        {
+            var ret = true;
+            var basePath = Path.GetDirectoryName(Application.ExecutablePath) ?? "";
+            var requires = new[]
+            {
+                ("Jil", "https://www.nuget.org/api/v2/package/Jil/", "2.15.4"),
+                ("Sigil", "https://www.nuget.org/api/v2/package/Sigil/", "4.7.0")
+            };
+            foreach (var require in requires)
+            {
+                var dllPath = Path.Combine(basePath, require.Item1 + ".dll");
+                if (File.Exists(dllPath)) continue;
+                Notification.ShowInfo($"缺少{require.Item1} {require.Item3}");
+                ret = false;
+            }
+            return ret;
         }
     }
 }
