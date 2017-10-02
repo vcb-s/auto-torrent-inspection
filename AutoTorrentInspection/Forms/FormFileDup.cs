6df4728c97b9535a45a0657c6d14e7cd0db32657
tautcony <tautcony@gmail.com>
2017-10-02 12:38:55 +0800

    Cancel the async method while closing the form.

--- AutoTorrentInspection/Forms/FormFileDup.cs
+++ AutoTorrentInspection/Forms/FormFileDup.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 using AutoTorrentInspection.Util;
 using AutoTorrentInspection.Util.Crc32.NET;
@@ -11,19 +11,23 @@ namespace AutoTorrentInspection.Forms
 {
     public partial class FormFileDup: Form
     {
+        private readonly CancellationTokenSource _cts;
+
         public FormFileDup()
         {
             InitializeComponent();
+            Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
         }
 
         public FormFileDup(IEnumerable<(long, IEnumerable<FileDescription>)> sizeData)
         {
             InitializeComponent();
             Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
-            GetCRCAsync(sizeData);
+            _cts = new CancellationTokenSource();
+            GetCRCAsync(sizeData, _cts.Token);
         }
 
-        private async void GetCRCAsync(IEnumerable<(long length, IEnumerable<FileDescription> files)> sizeData)
+        private async void GetCRCAsync(IEnumerable<(long length, IEnumerable<FileDescription> files)> sizeData, CancellationToken token)
         {
             var ret = sizeData.Select(size => new
             {
@@ -35,7 +39,8 @@ namespace AutoTorrentInspection.Forms
                 })
             });
             var treenode = treeView1.Nodes;
-            try {
+            try
+            {
                 foreach (var size in ret)
                 {
                     Logger.Log($"{size.filesize}:");
@@ -43,6 +48,7 @@ namespace AutoTorrentInspection.Forms
                     var tmp = new List<uint>();
                     foreach (var file in size.files)
                     {
+                        token.ThrowIfCancellationRequested();
                         var crc = await file.crc;
                         tmp.Add(crc);
                         if (crc == 0) node.Nodes.Add(file.info.ReletivePath + file.info.FileName);
@@ -55,17 +61,28 @@ namespace AutoTorrentInspection.Forms
                     var valid = tmp.Distinct().Count() != tmp.Count;
                     if (valid) treenode.Add(node);
                 }
-                treeView1.Sort();
-                treeView1.ExpandAll();
                 if (treenode.Count == 0)
                     Notification.ShowInfo(@"没有出现雷同的文件");
+            }
+            catch (OperationCanceledException exception)
+            {
+                Logger.Log(exception);
             }
             catch (Exception exception)
             {
                 Logger.Log(exception);
                 Notification.ShowError("Exception catched in GetCRCAsync", exception);
-                Close();
             }
+            finally
+            {
+                treeView1.Sort();
+                treeView1.ExpandAll();
+            }
+        }
+
+        private void FormFileDup_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _cts.Cancel();
         }
     }
 }
